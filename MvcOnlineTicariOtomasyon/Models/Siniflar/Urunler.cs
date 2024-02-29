41a505cd5577c12b56398cc7281a2bbd2b5ec630
Mehmethan Bellek <bellekmehmethan@gmail.com>
2024-03-01 00:06:52 +0300

    created Cariler and Personel classes

--- MvcOnlineTicariOtomasyon/Models/Siniflar/Urunler.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Urunler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,14 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
 {
     public class Urunler
     {
+        [Key]
         public int Urunid { get; set; }
+        public string UrunAd { get; set; }
+        public string Marka { get; set; }
+        public short Stok { get; set;}
+        public decimal AlisFiyati { get; set;}
+        public decimal SatisFiyati { get; set;}
+        public bool Durum {  get; set; }
+        public string UrunGorsel { get; set; }
     }
 }
