d9866a81a930280393e1f90a11558a0e7fa1dd68
Mehmethan Bellek <bellekmehmethan@gmail.com>
2024-02-29 23:16:42 +0300

    Kategori class created

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Kategori.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; // key kutuphanesi
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Kategori
+    {
+        //burada bir tablo olusturacagiz bunu belirtiyoruz sutunlarin satirlarini olacagini soyluyoruz
+        //degisken degil propertyler olacak
+
+        //entityler uzerinden uygulamamizi yapicaz bu yuzden keye ihtiyacimiz o da primary key
+        [Key]
+        //property erisim belirleyici
+        public int KategoriId { get; set; }
+        public string KategoriAd {  get; set; }
+    }
+}
