9ae5bce90daab4c78c00a1c747bf85eff54f786b
Mehmethan Bellek <bellekmehmethan@gmail.com>
2024-05-22 23:50:56 +0300

    KategoriEkle() method added

--- MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
@@ -16,5 +16,16 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             var degerler = c.Kategoris.ToList();
             return View(degerler);
         }
+
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+
+        //we added second time because KategoriEkle() method run when the page was uploaded
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
     }
 }
